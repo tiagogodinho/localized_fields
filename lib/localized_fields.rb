4eb4bd505d4206e348c04fa709ab6a5da8da0504
Tiago Rafael Godinho <tiagogodinho3@gmail.com>
2012-01-05 23:35:52 -0200

    Transferring LocalizedFields::FormHelpers to a separate file inside Helpers namespace.

--- lib/localized_fields.rb
+++ lib/localized_fields.rb
@@ -7,26 +7,7 @@ module LocalizedFields
   extend ActiveSupport::Autoload
   
   autoload :FormBuilder
-    
-  module FormHelpers
-    def localized_fields(field = nil, options = {}, &block)
-      if field
-        field_name = "#{field}_translations"
-        object = @object.send(field_name)
-        name = "#{object_name}[#{field_name}]"
-        
-        @template.fields_for(name, object, options.merge(:builder => LocalizedFields::FormBuilder), &block).html_safe
-      else
-        output = ''
-        
-        I18n.available_locales.each do |language|
-          output << @template.fields_for(object_name, @object, options.merge(:builder => LocalizedFields::FormBuilder, :language => language), &block)
-        end
-        
-        output.html_safe
-      end
-    end
-  end
+  autoload :Helpers
 end
 
-ActionView::Helpers::FormBuilder.send :include, LocalizedFields::FormHelpers
+ActionView::Helpers::FormBuilder.send :include, LocalizedFields::Helpers::FormHelper
---
+++ lib/localized_fields/helpers/form_helper.rb
@@ -0,0 +1,23 @@
+module LocalizedFields
+  module Helpers
+    module FormHelper
+      def localized_fields(field = nil, options = {}, &block)
+        if field
+          field_name = "#{field}_translations"
+          object = @object.send(field_name)
+          name = "#{object_name}[#{field_name}]"
+        
+          @template.fields_for(name, object, options.merge(:builder => LocalizedFields::FormBuilder), &block).html_safe
+        else
+          output = ''
+          
+          I18n.available_locales.each do |language|
+            output << @template.fields_for(object_name, @object, options.merge(:builder => LocalizedFields::FormBuilder, :language => language), &block)
+          end
+          
+          output.html_safe
+        end
+      end
+    end
+  end
+end
