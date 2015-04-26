37ca8ff78abaa5f67a76a6d3cffd0d477a4c6e4e
Johnny Shields <johnny.shields@gmail.com>
2015-04-26 18:48:29 +0900

    Fix specs for Rails 4 and get Travis CI to pass.
    I've now added gemfiles for each of Rails versions 3.2, 4.0, 4.1, 4.2. In addition, there's a "master" version but Mongoid currently conflicts so it's set to Rails 4.2 and Mongoid master. I've moved /gemfiles to be /spec/gemfiles.
    
    Rails 3 specs are marked as "allowed failures" since the format has changed entirely. I've investigated and decided that it's not worth the effort to make both Rails 3 and 4 pass, but at least I've gotten to the point where the Rails 3 env build correctly and we can see the individual specs fail due to format change.
    
    I've also added coveralls as a dev dependency, otherwise Travis fails.
    
    Lastly, Travis CI does not require service: mongodb to pass hence I've removed it.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,11 @@ Coveralls.wear!
 require 'localized_fields'
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))].each { |f| require f }
+
+def rails3?
+  !defined?(ActionView::VERSION::MAJOR)
+end
+
+def rails4?
+  ActionView::VERSION::MAJOR == 4
+end
